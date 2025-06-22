f9375306139c285eccf6f9f0581c6abbc05e0aad
Shuvajit Kumar Das <shuvajitdas838@gmail.com>
2025-06-22 19:34:53 +0600

    types added

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,8 @@
 import app from './app';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-
-
-
-dotenv.config();
 const PORT = 5000
 async function startServer() {
-    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.hc9ux7i.mongodb.net/library-management-app?retryWrites=true&w=majority&appName=Cluster1`);
+    await mongoose.connect(`mongodb+srv://shuvogitlab2024:shuvo%4012131@cluster1.hc9ux7i.mongodb.net/library-management-app?retryWrites=true&w=majority&appName=Cluster1`);
     console.log("server is connected through mongoose");
     app.listen(PORT, () => {
         console.log(`app is listening to port ${PORT}`);
